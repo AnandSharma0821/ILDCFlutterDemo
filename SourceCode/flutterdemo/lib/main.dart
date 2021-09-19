d584c04e33f7e27ffb6f7b3c4ac6da704b20f029
AnandSharma0821 <71649978+AnandSharma0821@users.noreply.github.com>
2021-09-19 23:29:01 +0530

    Assignment 7
    Assignment 7

--- SourceCode/flutterdemo/lib/main.dart
+++ SourceCode/flutterdemo/lib/main.dart
@@ -21,93 +21,420 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        canvasColor: Colors.blue[100],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LaunchScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LaunchScreen extends StatefulWidget {
+  LaunchScreen({Key? key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _LaunchScreenState createState() => _LaunchScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _LaunchScreenState extends State<LaunchScreen> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Container(
+      color: Colors.white,
+      child: Column(
+          //  mainAxisAlignment: MainAxisAlignment.start,
+          //  crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            Padding(padding: EdgeInsets.all(screenHeight * 0.02)),
+            generateFirstRow(context), // First Row design
+            Padding(padding: EdgeInsets.all(screenHeight * 0.005)),
+            generateSecondRow(context), // Second Row design
+            Padding(padding: EdgeInsets.all(screenHeight * 0.01)),
+            generateThirdRow(context), // Third Row design
+            Padding(padding: EdgeInsets.all(screenHeight * 0.03)),
+            generateFourthRow(context), // Fourth Row design
+            Padding(padding: EdgeInsets.all(screenHeight * 0.01)),
+            generateFifthRow(context), // Fifth Row Design
+            Padding(padding: EdgeInsets.all(screenHeight * 0.02)),
+            generateSixthRow(context), // Sixth Row Design
+            Padding(padding: EdgeInsets.all(screenHeight * 0.01)),
+            generateSeventhRow(context), // Seventh Row Design
+            Padding(padding: EdgeInsets.all(screenHeight * 0.02)),
+            generateEigthRow(context), // Eight Row Design
+            Padding(padding: EdgeInsets.all(screenHeight * 0.02)),
+            generateNinthRow(context), // Ninth Row Design
+          ]),
     );
   }
+}
+
+// generateFirstRow: Call this method to generate first row
+FractionallySizedBox generateFirstRow(BuildContext context) {
+  double screenWidth = MediaQuery.of(context).size.width;
+  double screenHeight = MediaQuery.of(context).size.height;
+  return FractionallySizedBox(
+    widthFactor: 0.9,
+    child: Container(
+      child: Row(
+        children: [
+          Container(
+            width: screenWidth * 0.45,
+            decoration: BoxDecoration(
+                color: Color(0xff610C00),
+                border: Border.all(width: 1, color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+          ),
+          Container(
+            width: screenWidth * 0.05,
+          ),
+          Container(
+            width: screenWidth * 0.40,
+            decoration: BoxDecoration(
+                color: Color(0xff325805),
+                border: Border.all(width: 1, color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+          ),
+        ],
+      ),
+      height: screenHeight * 0.18,
+    ),
+  );
+}
+
+// generateSecondRow: Call this method to generate second row
+FractionallySizedBox generateSecondRow(BuildContext context) {
+  double screenWidth = MediaQuery.of(context).size.width;
+  double screenHeight = MediaQuery.of(context).size.height;
+  return FractionallySizedBox(
+    widthFactor: 0.9,
+    child: Container(
+      child: Row(
+        children: [
+          Container(
+            width: screenWidth * 0.26,
+            decoration: BoxDecoration(
+                color: Color(0xff294F95),
+                border: Border.all(width: 1, color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+          ),
+          Container(
+            width: screenWidth * 0.05,
+          ),
+          Container(
+            width: screenWidth * 0.26,
+            decoration: BoxDecoration(
+                color: Color(0xff231259),
+                border: Border.all(width: 1, color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+          ),
+          Container(
+            width: screenWidth * 0.07,
+          ),
+          Container(
+            width: screenWidth * 0.26,
+            decoration: BoxDecoration(
+                color: Color(0xff520F37),
+                border: Border.all(width: 1, color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+          ),
+        ],
+      ),
+      height: screenHeight * 0.15,
+    ),
+  );
+}
+
+// generateThirdRow: Call this method to generate third row
+FractionallySizedBox generateThirdRow(BuildContext context) {
+  double screenWidth = MediaQuery.of(context).size.width;
+  double screenHeight = MediaQuery.of(context).size.height;
+  return FractionallySizedBox(
+    widthFactor: 0.9,
+    child: Container(
+      child: Row(
+        children: [
+          Container(
+            width: screenWidth * 0.60,
+            decoration: BoxDecoration(
+                color: Color(0xff7E4700),
+                border: Border.all(width: 1, color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+          ),
+          Container(
+            width: screenWidth * 0.05,
+          ),
+          Container(
+            width: screenWidth * 0.25,
+            decoration: BoxDecoration(
+                color: Color(0xff876B00),
+                border: Border.all(width: 1, color: Colors.black),
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+          ),
+        ],
+      ),
+      height: screenHeight * 0.15,
+    ),
+  );
+}
+
+// generateFourthRow: Call this method to generate fourth row
+FractionallySizedBox generateFourthRow(BuildContext context) {
+  double screenWidth = MediaQuery.of(context).size.width;
+  double screenHeight = MediaQuery.of(context).size.height;
+  return FractionallySizedBox(
+    widthFactor: 0.9,
+    child: Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.baseline,
+        textBaseline: TextBaseline.ideographic,
+        children: [
+          Padding(padding: EdgeInsets.all(screenWidth * 0.01)),
+          Container(
+            width: screenWidth * 0.26,
+            child: Text(
+              'Fourth',
+              style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              maxLines: 1,
+            ),
+          ),
+          Container(
+            width: screenWidth * 0.20,
+            child: Text(
+              'Fourth',
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              maxLines: 1,
+              textDirection: TextDirection.rtl,
+            ),
+          ),
+          Container(
+            width: screenWidth * 0.07,
+          ),
+          Container(
+            width: screenWidth * 0.32,
+            child: Text(
+              'Fourth',
+              style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              maxLines: 1,
+            ),
+          ),
+        ],
+      ),
+      height: screenHeight * 0.04,
+    ),
+  );
+}
+
+// generateFifthRow: Call this method to generate fifth row
+FractionallySizedBox generateFifthRow(BuildContext context) {
+  return FractionallySizedBox(
+    widthFactor: 0.8,
+    child: Container(
+      color: Colors.black,
+      height: 1,
+    ),
+  );
+}
+
+// generateSixthRow: Call this method to generate sixth row
+FractionallySizedBox generateSixthRow(BuildContext context) {
+  return FractionallySizedBox(
+    widthFactor: 0.9,
+    child: Row(
+      children: [
+        Container(
+          color: Colors.black,
+          height: 1,
+        ),
+        Text(
+          'Fifth',
+          style: TextStyle(
+              fontSize: 26,
+              color: Colors.black,
+              decoration: TextDecoration.none),
+          maxLines: 1,
+        ),
+      ],
+    ),
+  );
+}
+
+// generateSeventhRow: Call this method to generate seventh row
+FractionallySizedBox generateSeventhRow(BuildContext context) {
+  double screenWidth = MediaQuery.of(context).size.width;
+  double screenHeight = MediaQuery.of(context).size.height;
+  return FractionallySizedBox(
+    widthFactor: 0.9,
+    child: Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.baseline,
+        textBaseline: TextBaseline.ideographic,
+        children: [
+          Container(
+            width: screenWidth * 0.26,
+            child: Text(
+              'Sixth',
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              maxLines: 1,
+            ),
+          ),
+          Container(
+            width: screenWidth * 0.30,
+            child: Text(
+              'Sixth',
+              style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              maxLines: 1,
+              textDirection: TextDirection.rtl,
+            ),
+          ),
+          Container(
+            width: screenWidth * 0.07,
+          ),
+          Container(
+            width: screenWidth * 0.27,
+            child: Text(
+              '',
+              style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              maxLines: 1,
+            ),
+          ),
+        ],
+      ),
+      height: screenHeight * 0.04,
+    ),
+  );
+}
+
+// generateSeventhRow: Call this method to generate seventh row
+FractionallySizedBox generateEigthRow(BuildContext context) {
+  double screenWidth = MediaQuery.of(context).size.width;
+  double screenHeight = MediaQuery.of(context).size.height;
+  return FractionallySizedBox(
+    widthFactor: 1,
+    child: Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.baseline,
+        textBaseline: TextBaseline.ideographic,
+        children: [
+          Container(
+            width: screenWidth * 0.18,
+            child: Text(
+              '',
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              maxLines: 1,
+            ),
+          ),
+          Container(
+            width: screenWidth * 0.40,
+            child: Text(
+              'Seventh',
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              maxLines: 1,
+              textDirection: TextDirection.rtl,
+            ),
+          ),
+          Container(
+            width: screenWidth * 0.07,
+          ),
+          Container(
+            width: screenWidth * 0.34,
+            child: Text(
+              'Seventh',
+              style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              maxLines: 1,
+              textDirection: TextDirection.rtl,
+            ),
+          ),
+        ],
+      ),
+      height: screenHeight * 0.04,
+    ),
+  );
+}
+
+// generateNinthRow: Call this method to generate ninth row
+FractionallySizedBox generateNinthRow(BuildContext context) {
+  double screenWidth = MediaQuery.of(context).size.width;
+  double screenHeight = MediaQuery.of(context).size.height;
+  return FractionallySizedBox(
+    widthFactor: 1,
+    child: Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.baseline,
+        textBaseline: TextBaseline.ideographic,
+        children: [
+          Container(
+            width: screenWidth * 0.26,
+            child: Text(
+              '',
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              maxLines: 1,
+            ),
+          ),
+          Container(
+            width: screenWidth * 0.20,
+            child: Text(
+              '',
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+              maxLines: 1,
+              textDirection: TextDirection.rtl,
+            ),
+          ),
+          Container(
+            width: screenWidth * 0.07,
+          ),
+          Container(
+            width: screenWidth * 0.45,
+            child: Text(
+              'Eight',
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                  decoration: TextDecoration.none),
+                  textDirection: TextDirection.rtl,
+              maxLines: 1,
+            ),
+          ),
+        ],
+      ),
+      height: screenHeight * 0.04,
+    ),
+  );
 }
